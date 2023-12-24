4254085d5151addc67b302c6dfe1c00c4620d196
Idris Mikayil <idrisrm@code.edu.az>
2023-12-24 19:17:56 +0400

    edit

---
+++ Domain/Entities/Home/HomeAbout.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entities.Home
+{
+    public class HomeAbout
+    {
+        public string UpTitle { get; set; }
+        public string MainTitle { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        public string Image { get; set; }
+        public string ImageTitle { get; set; }
+        public int Number { get; set; }
+    }
+}
