6ac7f62273c4f9967ba00a385c5b47a4214452a8
Elnur <63106963+KomoGit@users.noreply.github.com>
2023-12-25 14:13:58 +0400

    Adjustments

--- Application/PageDetails/PartnerPageDetails/Handlers/GetAllPartnersHandler.cs
+++ Application/PageDetails/PartnerPageDetails/Handlers/GetAllPartnersHandler.cs
@@ -1,20 +1,17 @@
-﻿using Application.PageDetails.PartnerPageDetails.Commands;
-using Application.PageDetails.PartnerPageDetails.Responses;
+﻿using Application.PageDetails.PartnerPageDetails.Queries;
+using Domain.Entities;
 using Infrastructure.Data;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace Application.PageDetails.PartnerPageDetails.Handlers
 {
-    public class GetAllPartnersHandler(AppDbContext context) : IRequestHandler<GetAllPartnersCommand, List<GetAllPartnersResponse>>
+    public class GetAllPartnersHandle(AppDbContext context) : IRequestHandler<GetAllPartnersQuery, IEnumerable<Partner>>
     {
-        async Task<List<GetAllPartnersResponse>> IRequestHandler<GetAllPartnersCommand, List<GetAllPartnersResponse>>.Handle(GetAllPartnersCommand request, CancellationToken cancellationToken)
+        public async Task<IEnumerable<Partner>> Handle(GetAllPartnersQuery request, CancellationToken cancellationToken)
         {
-            return await context.Partners.Select(x => new GetAllPartnersResponse
-            {
-                Link = x.Link,
-                ImageLink = x.ImageTitle
-            }).ToListAsync(cancellationToken);
+            List<Partner> data = await context.Partners.ToListAsync(cancellationToken: cancellationToken);
+            return data.AsEnumerable();
         }
     }
 }
---
+++ Application/PageDetails/PartnerPageDetails/Queries/GetAllPartnersQuery.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+using MediatR;
+
+namespace Application.PageDetails.PartnerPageDetails.Queries
+{
+    public class GetAllPartnersQuery : IRequest<IEnumerable<Partner>>
+    {
+
+    }
+}
