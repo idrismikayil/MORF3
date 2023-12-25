d4444bcc1c023ad4acb3be6c9d1fead362f50acd
Elnur <63106963+KomoGit@users.noreply.github.com>
2023-12-25 14:14:21 +0400

    ++

---
+++ API/Controllers/PartnerController.cs
@@ -0,0 +1,19 @@
+﻿using Application.PageDetails.PartnerPageDetails.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PartnerController(IMediator mediator) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllPartners()
+        {
+            return Ok(await _mediator.Send(new GetAllPartnersQuery()));
+        }
+    }
+}
--- Infrastructure/Data/AppDbContext.cs
+++ Infrastructure/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Entities;
+﻿using Domain;
+using Domain.Entities;
 using Domain.Entities.Home;
 using Domain.Exceptions;
 using Microsoft.EntityFrameworkCore;
@@ -6,13 +7,20 @@ using Microsoft.Extensions.Configuration;
 
 namespace Infrastructure.Data
 {
-    public class AppDbContext(IConfiguration _configuration) : DbContext
+    public class AppDbContext(IConfiguration _configuration, DbContextOptions<AppDbContext> options) : DbContext(options) 
     {
-        public DbSet<User> Users { get; set; }
-        public DbSet<Partner> Partners { get; set; }
-        public DbSet<Portfolio> Portfolios { get; set; }
+        #region Home
         public DbSet<Banner> Banners { get; set; }
         public DbSet<Statistic> Statistics { get; set; }
+        public DbSet<Partner> Partners { get; set; }
+        #endregion
+        #region Footer
+        public DbSet<Address> Addresses { get; set; }
+        #endregion
+        public DbSet<User> Users { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        
+        
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
